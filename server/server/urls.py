be610c4122d8fe41017c3238523eefffca90d849
Akshat Sharma <akshatsharma2602@gmail.com>
2022-04-21 18:47:17 +0530

    Added sentry sdk

--- server/server/urls.py
+++ server/server/urls.py
@@ -20,10 +20,12 @@ from django.conf.urls import url
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 
 
 schema_view = get_schema_view(
@@ -40,6 +42,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('sentry-debug/', trigger_error),
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
