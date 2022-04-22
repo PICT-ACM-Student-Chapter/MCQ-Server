a639a455f0c041e6919cb04e55f93df2c7721811
Akshat Sharma <akshatsharma2602@gmail.com>
2022-04-22 10:22:42 +0530

    Added csv functionality while in prod

--- server/core/admin.py
+++ server/core/admin.py
@@ -6,6 +6,7 @@ from django.db import models
 from martor.widgets import AdminMartorWidget
 from django.contrib.auth.admin import UserAdmin
 import uuid
+import pandas as pd
 
 from .models import Question, User_Event, User_Question, Event, User_Result, User
 # Register your models here.
@@ -14,9 +15,6 @@ class CsvImp(forms.Form):
 
 class MyAdmin(admin.ModelAdmin):
     change_list_template = 'change_list.html'
-    list_display=('id','statement','fk_event')
-    list_filter=('fk_event',)
-    search_fields=('id','statement','fk_event')
     def get_urls(self):
         urls=super().get_urls()
         new_urls=[path('upload-csv/',self.upload_csv)]
@@ -24,16 +22,17 @@ class MyAdmin(admin.ModelAdmin):
     def upload_csv(self,request):
         if request.method=='POST':
             csv_file=request.FILES["csv_upload"]
-            file_data=csv_file.read().decode("utf-8")
-            csv_data=file_data.split('\n')
+            csv_data=pd.read_csv(csv_file, '|')
+            # csv_data=file_data.split('\n')
             for x in range(1,len(csv_data)-2):
-                fields=csv_data[x].split("|")
+                # fields=csv_data[x].split("|")
                 try:
                     created=Question.objects.update_or_create(
                         # id=fields[0],
-                        statement=fields[0],
-                        options=[fields[1],fields[2],fields[3],fields[4]],
-                        correct_option=fields[5],
+                        statement=csv_data.iloc[x][0],
+                        options=[csv_data.iloc[x][1],csv_data.iloc[x][2],csv_data.iloc[x][3],csv_data.iloc[x][4]],
+                        correct_option=csv_data.iloc[x][5],
+                        fk_event=Event.objects.get(id=uuid.UUID("ecdacee2-51ce-4a20-be34-6abf8cab1217")),
                     )
                 except:
                     pass
@@ -57,7 +56,6 @@ class CustomUserAdmin(UserAdmin):
             },
         ),
     )
-
 admin.site.register(User, CustomUserAdmin)
 
 admin.site.register(Question,MyAdmin)
